b317bf1aefd96599131312ec4ae7d4f050601ba5
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-04 14:24:22 -0700

    react router dom example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  const PageOne = () => <div>PAGE ONE</div>;
+  const PageTwo = () => (
+    <div>
+      PAGE TWO
+      <button>Click ME</button>
+    </div>
+  );
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
