a708a8c791bfc896cc8cd04c970c2444c48d0c22
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-05 15:39:44 -0700

    connect header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
+import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
@@ -23,7 +24,8 @@ const App = () => {
     </div>
   );
   return (
-    <div>
+    <div className="ui component">
+      <Header />
       <BrowserRouter>
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" component={StreamCreate} />
