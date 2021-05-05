e73f701304a230787b8c29b664d0c27deac59df4
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-05 15:28:13 -0700

    wiring routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
+import StreamCreate from "./streams/StreamCreate";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+
 const App = () => {
   const PageOne = () => (
     <div>
@@ -19,8 +25,11 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" component={PageTwo} />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/show" component={StreamShow} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
       </BrowserRouter>
     </div>
   );
