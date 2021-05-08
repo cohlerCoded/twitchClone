9dd5bd55e83d62b64e3a8d6bfd889246a5697f2d
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-07 17:25:56 -0700

    auth reducer

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,16 @@
+import { signIn, signOut } from "../actions";
+
+const INITIAL_STATE = {
+  isSignedIn: null,
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SIGN_IN":
+      return { ...state, isSignedIn: true };
+    case "SIGN_OUT":
+      return { ...state, isSignedIn: false };
+    default:
+      return state;
+  }
+};
