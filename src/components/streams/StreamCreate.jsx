3aee68785445dda96582b5b882db3f7a2b01b560
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-09 23:23:53 -0700

    my guess approach

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+  renderTitleInput() {
+    return (
+      <>
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input type="text" name="title" id="title" />
+        </div>
+      </>
+    );
+  }
+  renderDescriptionInput() {
+    return (
+      <>
+        <div>
+          <label htmlFor="description">Description: </label>
+          <input type="text" name="description" id="description" />
+        </div>
+      </>
+    );
+  }
   render() {
     return (
       <div>
         <form action="">
-          <Field name="title" />
-          <Field name="description" />
+          <Field name="title" component={this.renderTitleInput} />
+          <Field name="description" component={this.renderDescriptionInput} />
         </form>
       </div>
     );
