9a6132c01840924347d104adf3df90a2892b3075
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-11 15:04:38 -0700

    dispatch actions after stream creation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import { formValues } from "redux-form";
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
 
 export const signIn = (userId) => {
   return { type: SIGN_IN, payload: userId };
@@ -9,5 +9,6 @@ export const signOut = () => {
   return { type: SIGN_OUT };
 };
 export const createStream = (formValues) => async (dispatch) => {
-  streams.post("/streams", formValues);
+  const res = await streams.post("/streams", formValues);
+  dispatch({ type: CREATE_STREAM, payload: res.data });
 };
