c1e1ed780ccd6c40d6d14da5ebc09aaa261b2c37
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-11 14:14:54 -0700

    create stream thru action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { formValues } from "redux-form";
+import streams from "../apis/streams";
 import { SIGN_IN, SIGN_OUT } from "./types";
 
 export const signIn = (userId) => {
@@ -5,4 +7,7 @@ export const signIn = (userId) => {
 };
 export const signOut = () => {
   return { type: SIGN_OUT };
+};
+export const createStream = (formValues) => async (dispatch) => {
+  streams.post("/streams", formValues);
 };
