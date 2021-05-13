dc49cf5d086a559fe53bece4822c2384f8b3878e
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-13 14:09:24 -0700

    conditionally rndering buttons

--- src/components/streams/StreamList.jsx
+++ src/components/streams/StreamList.jsx
@@ -6,10 +6,23 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderButtons(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative"> Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderButtons(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content header">
             {stream.title}
@@ -29,6 +42,9 @@ class StreamList extends React.Component {
   }
 }
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+  };
 };
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
