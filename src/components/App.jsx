28e60db82b7b0728d4a81681a9222847e5da0b95
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-14 16:40:28 -0700

    wildcard nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" component={StreamCreate} />
         <Route path="/streams/show" component={StreamShow} />
-        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/edit/:id" component={StreamEdit} />
         <Route path="/streams/delete" component={StreamDelete} />
       </Router>
     </div>
